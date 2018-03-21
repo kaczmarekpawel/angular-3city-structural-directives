d50c71c46d26c408e7e9aecf7e70899c3594f6a5
pawel.kaczmarek <pawel.kaczmarek.85@gmail.com>
2018-03-21 22:23:01 +0100

    Angular 3city structural directives - assets, cleanup

--- src/app/RolesChecker/roles-checker.component.ts
+++ src/app/RolesChecker/roles-checker.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AuthService, Role} from '../Auth';
 
 @Component({
   selector: 'app-roles-checker-component',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AuthService, HasRoleDirective} from './Auth';
+import {AuthService} from './Auth';
 import {RolesCheckerComponent} from './RolesChecker';
 import {
   ForSampleComponent,
@@ -22,7 +22,6 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     ForSampleComponent,
-    HasRoleDirective,
     IfSampleComponent,
     RolesCheckerComponent,
     PredefinedDirectivesComponent
