1bd67748181cd36b9bb4d293723170d8368c8f87
pawel.kaczmarek <pawel.kaczmarek.85@gmail.com>
2018-03-21 22:23:58 +0100

    Angular 3city structural directives - has role with as an NgIf extension

--- src/app/Auth/hasRole.directive.ts
+++ src/app/Auth/hasRole.directive.ts
@@ -1,19 +1,39 @@
 import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {NgIf} from '@angular/common';
 import {AuthService} from './auth.service';
 import {Role} from './auth.types';
 
+
+// @Directive({selector: '[appHasRole]'})
+// export class HasRoleDirective {
+//   constructor(private authService: AuthService,
+//               private templateRef: TemplateRef<any>,
+//               private viewContainer: ViewContainerRef) {
+//   }
+//
+//   @Input() set appHasRole1(role: Role) {
+//     if (this.authService.hasRole(role)) {
+//        this.viewContainer.createEmbeddedView(this.templateRef);
+//     } else {
+//       this.viewContainer.clear();
+//     }
+//   }
+// }
+
+
 @Directive({selector: '[appHasRole]'})
-export class HasRoleDirective {
+export class HasRoleDirective extends NgIf {
   constructor(private authService: AuthService,
               private templateRef: TemplateRef<any>,
               private viewContainer: ViewContainerRef) {
+    super(viewContainer, templateRef);
   }
 
   @Input() set appHasRole(role: Role) {
-    if (this.authService.hasRole(role)) {
-       this.viewContainer.createEmbeddedView(this.templateRef);
-    } else {
-      this.viewContainer.clear();
-    }
+    this.ngIf = this.authService.hasRole(role);
+  }
+
+  @Input() set appHasRoleElse(templateRef: TemplateRef<any>) {
+    this.ngIfElse = templateRef;
   }
 }
